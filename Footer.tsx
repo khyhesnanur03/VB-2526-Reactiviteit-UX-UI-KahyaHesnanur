58ea76cdc25c5e225a77725d293fc2f42555a229
khyhesnanur03 <hesnanurkahya0303@hotmail.com>
2026-04-28 14:26:47 +0200

    Add files via upload

---
+++ Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer className="w-full bg-black py-6">
+      <div className="max-w-6xl mx-auto px-8 text-center text-sm text-white">
+        <p>© 2025 De Standaard Redesign Project</p>
+        <p>UX/UI Design by Kahya Hesnanur • 1GDM1</p>
+      </div>
+    </footer>
+  );
+}
